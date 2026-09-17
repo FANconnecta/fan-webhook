e7b4c87c383a2e03a06eb23fadda280f64a38ebc
FANconnecta <mike@falselyaccusednetwork.co.uk>
2026-09-17 22:11:41 +0100

    Add Home component to page.js

---
+++ app/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: 40 }}>
+      <h1>FAN Wix → HubSpot webhook</h1>
+      <p>This app is running.</p>
+    </main>
+  );
+}
